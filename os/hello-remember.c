011da105d45dd7d34997cf0b5dfc6d34c8ff5ba2
AlexandreVMArie <dabalexandra@yahoo.com>
2021-07-20 23:42:02 +0300

    Solve hello-remember

--- os/hello-remember.c
+++ os/hello-remember.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct node {
     char *name;
     struct node *next;
 };
 
-struct node *add(struct node head, char *name) {
+struct node *add(struct node* head, char *name) {
     struct node *n;
     struct node *p;
 
     n = (struct node *) malloc(sizeof(struct node));
-    n->name = (char *) malloc(strlen(name));
+    n->name = (char *) malloc(strlen(name) + 1);
     strcpy(n->name, name);
     n->next = NULL;
 
@@ -26,11 +28,11 @@ struct node *add(struct node head, char *name) {
     return head;
 }
 
-int known(struct node *head, char name) {
+int known(struct node *head, char* name) {
     struct node *p;
 
     p = head;
-    whilep != NULL && strcmp(p.name, name) != 0) {
+    while (p != NULL && strcmp(p->name, name) != 0) {
         p = p->next;
     }
     return p != NULL;
@@ -40,24 +42,23 @@ void clear(struct node *head) {
     if (head == NULL) {
         return;
     }
-    clearhead->next);
+    clear(head->next);
     free(head->name);
     free(head);
 }
 
 int main(int argc, char **argv) {
     char name[64];
-    struct node *head;
+    struct node *head = NULL;
 
-    while (scanf( % s", name) == 1) {
-    if (known(head, name)) {
-        printf("Still around %s, eh?\n", name);
-    } else {
-        head = add(head, name);
-        printf("Hello %s\n", name);
+    while (scanf("%s", name) == 1) {
+        if (head != NULL && known(head, name)) {
+            printf("Still around %s, eh?\n", name);
+        } else {
+            head = add(head, name);
+            printf("Hello %s\n", name);
+        }
     }
-}
-
-clear(head);
-return 0;
+    clear(head);
+    return 0;
 }
