c9bd001d74ba1ff7bf5d1fd129a1b9e88597e01c
AlexandreVMArie <dabalexandra@yahoo.com>
2021-07-21 19:27:13 +0300

    Solve lab2_4

--- os/lab2_4.c
+++ os/lab2_4.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
 	long t[10], *p;
-
 	int i;
-	for (i=0; i<10; t[i++]=i);
-
-	p = t;
 
 	for (i=0; i<10; i++)
-		printf("%d %d %d %d\n", t[i], p[i], *(p+1), *(t+1));
+	    t[i] = i;
+
+    p = t;
+
+	for (i=0; i<10; i++)
+		printf("%ld %ld %ld %ld\n", t[i], p[i], *(p+1), *(t+1));
 
 	return 0;
 }
