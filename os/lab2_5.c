c8d1f6f37854931fdcc3c8aa2d5e796c86e1930f
AlexandreVMArie <dabalexandra@yahoo.com>
2021-07-21 19:32:15 +0300

    Solve lab2_5

--- os/lab2_5.c
+++ os/lab2_5.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
 	int n;
 
 	printf("Introduceti valoarea lui n: ");
-	scanf("%d", n);
+	scanf("%d", &n);
 
 	printf("Valoare lui n este: %d\n",n);
 	return 0;
